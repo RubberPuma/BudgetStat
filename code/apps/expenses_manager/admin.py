54777059e690abf8db01f1087d67192f502e5fa1
Cezary Stachurski <czareks20006@gmail.com>
2021-04-05 14:04:37 +0200

    Updated models

--- code/apps/expenses_manager/admin.py
+++ code/apps/expenses_manager/admin.py
@@ -3,6 +3,5 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Account)
 admin.site.register(Category)
 admin.site.register(Expense)
--- code/apps/expenses_manager/models.py
+++ code/apps/expenses_manager/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from apps.authentication.models import User
 
-class Account(models.Model):
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+class Expense(models.Model):
     CURRENCY_TYPE = [
         ('PLN', 'Polish Złoty'),
         ('EUR', 'Euro'),
@@ -11,18 +15,8 @@ class Account(models.Model):
         ('CHF', 'Swiss Franc'),
         ('CAD', 'Canadian Dollar'),
     ]
-    account_name = models.CharField(max_length=50)
-    currency = models.CharField(max_length=3, choices=CURRENCY_TYPE)
-    balance = models.DecimalField(max_digits=15, decimal_places=2)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-class Category(models.Model):
-    category_name = models.CharField(max_length=50)
-
-class Expense(models.Model):
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=3, choices=CURRENCY_TYPE)
     date = models.DateField()
