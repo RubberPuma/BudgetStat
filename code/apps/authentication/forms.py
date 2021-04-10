c39af9380529c88df5141ef9a10727f7293b509f
Cezary Stachurski <czareks20006@gmail.com>
2021-04-10 16:37:46 +0200

    Rebase

--- code/apps/authentication/forms.py
+++ code/apps/authentication/forms.py
@@ -9,10 +9,3 @@ class NewUserForm(UserCreationForm):
 	class Meta:
 		model = get_user_model()
 		fields = ("username", "email", "password1", "password2")
-
-	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
-		user.email = self.cleaned_data['email']
-		if commit:
-			user.save()
-		return user
--- code/apps/authentication/urls.py
+++ code/apps/authentication/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("register", views.register_request , name='register'),
     path("login", views.login_request, name='login'),
     path("logout", views.logout_request, name= "logout"),
+    # temporary url for testing
+    path("dashboard", views.dashboard, name= "dashboard"),
 ]
