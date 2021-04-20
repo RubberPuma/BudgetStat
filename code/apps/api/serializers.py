37426086990b2eb95f2a47e18e56e3610c9d33ee
Cezary Stachurski <czareks20006@gmail.com>
2021-04-20 22:39:44 +0200

    Updated LimitSerializer

--- code/apps/api/serializers.py
+++ code/apps/api/serializers.py
@@ -19,6 +19,7 @@ class ExpenseSerializer(serializers.ModelSerializer):
         fields = ['description', 'amount', 'currency', 'date', 'category']
 
 class LimitSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField(many=False)
     class Meta:
         model = Limit
-        fields = '__all__'
+        fields = ['limit_value', 'current_spent', 'period', 'start_date', 'category']
