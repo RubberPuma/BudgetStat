067ef7846e010790703da12a9eba23bbc534882a
Cezary Stachurski <czareks20006@gmail.com>
2021-04-20 22:29:29 +0200

    Created basic CRUD endpoints

--- code/apps/api/serializers.py
+++ code/apps/api/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from apps.api.models import Category
+from apps.api.models import Category, Expense, Limit
+from apps.authentication.models import User
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'category_name']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField(many=False)
+    class Meta:
+        model = Expense
+        fields = ['description', 'amount', 'currency', 'date', 'category']
+
+class LimitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Limit
+        fields = '__all__'
--- code/apps/api/urls.py
+++ code/apps/api/urls.py
@@ -4,6 +4,10 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'categories', views.CategoryViewSet)
+router.register(r'expenses', views.ExpenseViewSet)
+router.register(r'limits', views.LimitViewSet)
+router.register(r'users', views.UserViewSet)
+
 
 appname = 'api'
 urlpatterns = [
