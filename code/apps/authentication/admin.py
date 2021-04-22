41ec454b94983ab6e826ac4b0b92621c6ea71cc3
Cezary Stachurski <czareks20006@gmail.com>
2021-04-22 20:50:56 +0200

    make supercode

--- code/apps/authentication/admin.py
+++ code/apps/authentication/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 from django.forms import BaseInlineFormSet
 
 from apps.api.models import Expense, Limit
